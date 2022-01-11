0d6c49c3c43592639ecb19aef4870459ddc91549
Philip Eschenbacher <philip.eschenbacher@mongodb.com>
2022-01-11 17:48:41 +0100

    fixed collection name

--- fakeData/loadFakedata.sh
+++ fakeData/loadFakedata.sh
@@ -1,2 +1,2 @@
-mgeneratejs -n 50000 < fakeData/customerSingleView.json | mongoimport -u admin -p <password> -c customersSingleView mongodb+srv://mycustomers.<clusterId>.mongodb.net/MyCustomers
+mgeneratejs -n 50000 < fakeData/customerSingleView.json | mongoimport -u admin -p <password> -c customerSingleView mongodb+srv://mycustomers.<clusterId>.mongodb.net/MyCustomers
 mgeneratejs -n 100 < fakeData/consultantData.json | mongoimport -u admin -p <password> -c consultants mongodb+srv://mycustomers.<clusterId>.mongodb.net/MyCustomers
