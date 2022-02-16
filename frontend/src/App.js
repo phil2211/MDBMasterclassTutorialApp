eda39e5827ff0837c0376c009266592ae4dd05d0
Philip Eschenbacher <philip.eschenbacher@mongodb.com>
2022-02-16 19:17:04 +0100

    removed console.log

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { RealmAppProvider } from "./RealmApp";
 
 const APP_ID = process.env.REACT_APP_REALMAPP;
-console.log(process.env.REACT_APP_REALMAPP);
 
 const App = () => {
   return (
