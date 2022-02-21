5e7be049d2eb72067a9a8da3730969bd37b6b370
Philip Eschenbacher <philip.eschenbacher@mongodb.com>
2022-02-21 20:26:58 +0100

    some nice styling

---
+++ frontend/src/Components/FormRow.js
@@ -0,0 +1,15 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+const FormRow = ({ children }) => {
+    return (
+        <div css={css`
+            margin-bottom: 16px;
+        `}
+        >
+            {children}
+        </div>
+    );
+}
+
+export default FormRow;
