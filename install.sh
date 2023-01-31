e58ba11ce8fb391a549797f4c74133ee8c6d80c8
Philip Eschenbacher <philip.eschenbacher@mongodb.com>
2023-01-31 18:19:43 +0100

    added install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+atlas auth login -P MDBMasterclass && \
+atlas projects create MDBMasterclass -P MDBMasterclass && \
+atlas config set -P MDBMasterclass project_id `atlas project ls | grep MDBMasterclass | awk '{ print $1 }'` && \
+atlas quickstart --skipMongosh --skipSampleData --provider AWS --region EU_CENTRAL_1 --tier M0 --username admin --password Passw0rd --accessListIp "0.0.0.0/0" --clusterName MyCustomers -P MDBMasterclass --force && \
+sh testData/loadTestdata.sh admin Passw0rd $(atlas cluster connectionstrings describe MyCustomers -P MDBMasterclass | grep "mongodb+srv" | awk -F. '{print $2}') && \
+mongosh $(atlas cluster connectionstrings describe MyCustomers -P MDBMasterclass | grep "mongodb+srv")/MyCustomers --apiVersion 1 --username admin --password Passw0rd --eval 'db.customerSingleView.updateMany(
+    {},
+    [{
+      $set:
+      {
+        "totalBalance": {"$sum": "$accounts.balance"},
+        "age": {
+            "$subtract": [
+                {"$subtract": [{"$year": "$$NOW"}, {"$year": "$birthdate"}]},
+                {"$cond": [
+                    {"$gt": [0, {"$subtract": [{"$dayOfYear": "$$NOW"},{"$dayOfYear": "$birthdate"}]}]},
+                    1,
+                    0
+                ]}
+            ]
+        }
+      }
+    }]
+  );' && \
+atlas clusters search indexes create -P MDBMasterclass -f "testData/AtlasSearchDefinitions/customEnhanced.json" --clusterName MyCustomers && \
+atlas project apiKeys create --desc realm-cli --role GROUP_OWNER -P MDBMasterclass > AtlasAPIKeys.txt && \
+realm-cli login --api-key $(cat AtlasAPIKeys.txt | grep "Public API Key" | awk '{ print $4 }') --private-api-key $(cat AtlasAPIKeys.txt | grep "Private API Key" | awk '{ print $4 }') -y --profile MDBMasterclass && \
+realm-cli push --local "backend/MyCustomersGridApp" --include-package-json -y --profile MDBMasterclass && \
+echo "REACT_APP_REALMAPP="$(realm-cli apps list --profile MDBMasterclass | grep mycustomersgridapp | awk '{print $1}') > frontend/.env.local && \
+realm-cli users create --type email --email test@example.com --password Passw0rd --profile MDBMasterclass -a $(realm-cli apps list --profile MDBMasterclass | grep mycustomersgridapp | awk '{print $1}') && \
+echo "Please go to http://localhost:3000 and login with user: test@example.com and password: Passw0rd"
+cd frontend && npm install && npm start
